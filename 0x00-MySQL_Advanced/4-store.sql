a7fb53bc22320fcbb1cc75cfb644cbb852551fb8
Omar <omar0642624692@gmail.com>
2024-03-24 17:17:57 -0700

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,4 +4,5 @@ CREATE TRIGGER decrement
 AFTER INSERT
 ON orders
 FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
+UPDATE items SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;

32c548a517f186d66b79a3b3f17a3fef1f55b86c
Omar <omar0642624692@gmail.com>
2024-03-28 02:46:28 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
-"""A module with tools for request caching and tracking.
-"""
-import requests
+""" expiring web cache module """
+
 import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+r = redis.Redis()
 
 
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        r.incr(f"count:{url}")
+        cached_response = r.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        r.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
 def get_page(url: str) -> str:
     """get page
     """
-    r = redis.Redis()
-    r.incr(f"count:{url}")
-
-    cached_content = r.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
-
     response = requests.get(url)
-    page_content = response.text
-
-    r.setex(url, 10, page_content)
-    return page_content
+    return response.text

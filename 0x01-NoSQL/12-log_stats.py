2a7a51cc33829ed5a0f77798056771d2abfb9f3c
Omar <omar0642624692@gmail.com>
2024-03-30 09:00:17 -0700

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,25 +7,20 @@ from pymongo import MongoClient
 
 
 def log_stats():
-    """ log_stats.
+    """ provides some stats about Nginx logs stored in MongoDB
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    nginx_collection = client.logs.nginx
+    total = nginx_collection.count_documents({})
     print(f"{total} logs")
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        res = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {res}")
+    path = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
     print(f"{path} status check")
 
 

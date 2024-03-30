338f049314bba2ef13712a0d075f78c1eed57e6d
Omar <omar0642624692@gmail.com>
2024-03-30 11:20:53 -0700

    102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -6,28 +6,23 @@ from pymongo import MongoClient
 
 
 def log_stats():
-    """Prints stats about Nginx request logs.
+    """ log_stats.
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    nginx_collection = client.logs.nginx
+    total = nginx_collection.count_documents({})
     print(f"{total} logs")
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        res = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {res}")
+    path = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
     print(f"{path} status check")
     print("IPs:")
-    sorted_ips = logs_collection.aggregate(
+    sorted_ips = nginx_collection.aggregate(
         [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
          {"$sort": {"count": -1}}])
     i = 0
